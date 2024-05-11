9b9680651312b3d82a66959b40ee212e91bdbe67
kazimusaddirafi <kazirafi668@gmail.com>
2024-05-11 11:06:03 +0600

    Add project files.

---
+++ SignalRExample/wwwroot/js/userCount.js
@@ -0,0 +1,32 @@
+﻿//create connection
+var connectionuserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+//connect to methods that hub invoke
+connectionuserCount.on("updateTotalViews", (val) => {
+    var newCountSpan = document.getElementById("totalViewsCount")
+    newCountSpan.innerText = val.toString();
+})
+
+connectionuserCount.on("updateTotalUsers", (val) => {
+    var newCountSpan = document.getElementById("totalUsersCount")
+    newCountSpan.innerText = val.toString();
+})
+
+
+//invoke hub methods and send notification
+
+function newWindowLoadedOnClient() {
+    connectionuserCount.send("NewWindowLoaded");
+}
+
+//start
+function fulfilled() {
+    console.log('Connection success')
+    newWindowLoadedOnClient()
+}
+function rejected() {
+    console.log('Connection failed')
+}
+
+
+connectionuserCount.start().then(fulfilled, rejected)
